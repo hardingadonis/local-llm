70ab0f709da11a77caf82cf68011b9e9f12b1f11
Minh Vương <hardingadonis@gmail.com>
2023-11-30 16:45:23 +0700

    Update

--- frontend/js.js
+++ frontend/js.js
@@ -21,10 +21,7 @@ const chatContainer = document.getElementById('chat-container');
 
 const BOT_ENDPOINT = 'http://localhost:8000/v1';
 
-const messages = [{
-    role: 'system',
-    content: "Answer short and concise in 200 tokens only."
-}];
+const messages = [16:37 30/11/2023];
 
 function renderMessages() {
     chatContainer.innerHTML = '';
@@ -62,7 +59,7 @@ async function sendMessage() {
             body: JSON.stringify({
                 messages: messages,
                 temperature: 0.9,
-                max_tokens: 200,
+                max_tokens: 1000,
             }),
         });
 
